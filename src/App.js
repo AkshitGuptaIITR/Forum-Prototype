92a198b613d8ed425b147f519571e9d8c831d901
Akshit <Gakshit92@gmail.com>
2022-04-20 16:15:15 +0530

    adding routes and additon of header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import { useSelector } from 'react-redux';
+import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
+import "antd/dist/antd.css";
 
 function App() {
+  const { isAuth } = useSelector(state => state.user)
+
   return (
-    <div className="App">
-      <Header />
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Header />}>
+
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import style from "./Header.module.css"
+import React from "react";
+import style from "./Header.module.css";
+import { Input } from "antd";
+import { SearchOutlined, BellFilled, LoginOutlined } from "@ant-design/icons";
 
 const Header = () => {
   return (
-    <div className={style.headerStructure}>Header</div>
-  )
-}
+    <div className={style.headerStructure}>
+      <h1>Home</h1>
+      <Input
+        style={{ width: "40%" }}
+        size="large"
+        placeholder="Search"
+        prefix={<SearchOutlined />}
+      />
+      <div className={style.icons}>
+        <div>
+          <BellFilled style={{ fontSize: 20, marginRight: 8 }} />
+          Notification
+        </div>
+        <div>
+          <LoginOutlined style={{ fontSize: 20, marginRight: 8 }} />
+          Login
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
